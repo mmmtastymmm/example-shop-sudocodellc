fae3cd985a2730399f0452d6665fac7adb4399ff
Thomas Marks <tmsluigi@gmail.com>
2023-10-07 00:21:13 -0600

    Less offensive joke

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -68,7 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
       description: "For the astronaut in your life.",
     ),
     Product(
-      name: 'Woke Chess',
+      name: 'Updated Chess',
       price: 19.99 * 3,
       imageUrl:
           'https://images.unsplash.com/photo-1529699211952-734e80c4d42b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80',
