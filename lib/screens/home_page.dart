049b710c618eb3a296f6376feba8ce6e969b023a
Thomas Marks <tmsluigi@gmail.com>
2023-10-06 23:09:09 -0600

    Nicer floating button and have the description page now

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,13 +1,14 @@
 import 'package:example_shop_sudocodellc/providers/shopping_results.dart';
 import 'package:example_shop_sudocodellc/screens/login_page.dart';
+import 'package:example_shop_sudocodellc/screens/product_detail_screen.dart';
 import 'package:example_shop_sudocodellc/screens/signup_screen.dart';
 import 'package:example_shop_sudocodellc/screens/user_page.dart';
+import 'package:example_shop_sudocodellc/widgets/floating_shop_button.dart';
 import 'package:example_shop_sudocodellc/widgets/shift_right_fixer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../widgets/cart_item_widget.dart';
 import '../widgets/product_button.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -81,93 +82,6 @@ class _HomeScreenState extends State<HomeScreen> {
       description: "A nice little snack.",
     ),
   ];
-
-  void _showCheckoutAlert(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: const Text('Checkout Alert'),
-        content: const Column(
-          mainAxisSize: MainAxisSize.min,
-          // to make the column wrap its children
-          children: [
-            Icon(
-              Icons.sentiment_dissatisfied,
-              size: 60, // Adjust the size as needed
-              color: Colors.red, // Adjust the color as needed
-            ),
-            SizedBox(height: 20),
-            Text('This is just a demo so you can\'t actually buy these.'),
-          ],
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Okay'),
-            onPressed: () {
-              Navigator.of(ctx).pop();
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildBottomSheet(BuildContext context) {
-    List<Product> cartItems = Provider.of<ShoppingState>(context).cartItems;
-
-    return SizedBox(
-      height: 400,
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 50,
-            alignment: Alignment.center,
-            child: const Text(
-              'Your Cart',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-            ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  '${cartItems.length} Items',
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 16),
-                ),
-                Text(
-                  '\$${Provider.of<ShoppingState>(context).totalCartValue.toStringAsFixed(2)}',
-                  style: const TextStyle(color: Colors.green, fontSize: 16),
-                ),
-              ],
-            ),
-          ),
-          const Divider(),
-          Expanded(
-            child: ListView.builder(
-              itemCount: cartItems.length,
-              itemBuilder: (ctx, index) =>
-                  CartItemWidget(product: cartItems[index]),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: cartItems.isEmpty
-                ? const SizedBox()
-                : ElevatedButton(
-                    onPressed: () {
-                      _showCheckoutAlert(context);
-                    },
-                    child: const Text("Checkout"),
-                  ),
-          ),
-        ],
-      ),
-    );
-  }
 
   String _getNameFromUser(User? user) {
     if (user != null && user.displayName != null) {
@@ -262,7 +176,11 @@ class _HomeScreenState extends State<HomeScreen> {
                       child: ProductButton(
                         product: sampleProducts[index],
                         onPressed: () {
-                          print('Product ${sampleProducts[index].name} tapped');
+                          // Navigate to the product screen with the selected product
+                          Navigator.of(ctx).push(MaterialPageRoute(
+                            builder: (context) => ProductDetailScreen(
+                                product: sampleProducts[index]),
+                          ));
                         },
                       ),
                     ),
@@ -272,45 +190,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-              context: context,
-              builder: (context) => _buildBottomSheet(context),
-            );
-          },
-          tooltip: 'Shopping Cart',
-          child: Stack(
-            children: <Widget>[
-              const Icon(Icons.shopping_cart),
-              Positioned(
-                right: 0,
-                child: Container(
-                  padding: const EdgeInsets.all(1),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(6),
-                  ),
-                  constraints: const BoxConstraints(
-                    minWidth: 12,
-                    minHeight: 12,
-                  ),
-                  child: Text(
-                    Provider.of<ShoppingState>(context)
-                        .cartItems
-                        .length
-                        .toString(),
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 8,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        floatingActionButton: makeShopButton(context),
       ),
     );
   }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,5 +1,8 @@
+import 'package:example_shop_sudocodellc/widgets/floating_shop_button.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/shopping_results.dart';
 import '../widgets/product_button.dart';
 
 class ProductDetailScreen extends StatelessWidget {
@@ -16,11 +19,10 @@ class ProductDetailScreen extends StatelessWidget {
         title: Text(product.name),
       ),
       body: SingleChildScrollView(
-        // Ensures the screen is scrollable if the content overflows
         child: Column(
           children: <Widget>[
             SizedBox(
-              height: 300, // You can adjust this based on your preference
+              height: 300,
               width: double.infinity,
               child: Image.network(
                 product.imageUrl,
@@ -51,13 +53,23 @@ class ProductDetailScreen extends StatelessWidget {
                 textAlign: TextAlign.justify,
                 style: const TextStyle(
                   fontSize: 18,
-                  height: 1.5, // Adjusts line height for better readability
+                  height: 1.5,
                 ),
               ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Here's where you use Provider to add the product to cart
+                Provider.of<ShoppingState>(context, listen: false)
+                    .addToCart(product);
+              },
+              child: const Text('Add to Cart'),
             ),
           ],
         ),
       ),
+      floatingActionButton: makeShopButton(context),
     );
   }
 }
